2ba3680e74ae5cbb6465761b732125079a3dd23a
Viktor Kolind <s204166@student.dtu.dk>
2025-02-16 13:40:44 +0100

    made it first person

--- src/character/character.gd
+++ src/character/character.gd
@@ -9,6 +9,9 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		self.rotate_y( -deg_to_rad(event.relative.x * vertical_sensitivity))
@@ -29,7 +32,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
