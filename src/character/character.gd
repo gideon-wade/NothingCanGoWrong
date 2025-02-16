c54cf0d02db4e2f40c8b4a2ce21736dc9ecabd43
Viktor Kolind <s204166@student.dtu.dk>
2025-02-16 13:34:47 +0100

    added basic character controller, beaker art, basic map

---
+++ src/character/character.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+
+@export var vertical_sensitivity := 0.5
+@export var horizontal_sensitivity := 0.5
+
+@export var visuals : Node3D 
+@export var camera : Node3D
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		self.rotate_y( -deg_to_rad(event.relative.x * vertical_sensitivity))
+		camera.rotate_x(-deg_to_rad(event.relative.y * horizontal_sensitivity))
+		visuals.rotate_y(deg_to_rad(event.relative.x * horizontal_sensitivity))
+		var x_rotation = camera.rotation_degrees.x
+		x_rotation = clamp(x_rotation, -85, 50)  
+		camera.rotation_degrees.x = x_rotation
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
