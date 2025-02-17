90784100a571d249a1cd796038c4a6402de67bb1
Viktor Kolind <s204166@student.dtu.dk>
2025-02-17 21:00:10 +0100

    small fixes

--- src/objects/conical_flask/conical_flask.gd
+++ src/objects/conical_flask/conical_flask.gd
@@ -51,13 +51,12 @@ func set_color():
 	print("color changed ", color)
 	
 func _physics_process(delta):
-	if rotation_degrees.z > 90.0 and rotation_degrees.z < 270.0:
+	if is_facing_down():
 		draw_particles = true
 		raycast.enabled = true
 	else : 
 		draw_particles = false
 		raycast.enabled = false
-		
 	# pouring logic
 	raycast.look_at(raycast.global_transform.origin + Vector3(0, 0, -10), Vector3.UP)
 	var colider = raycast.get_collider()
@@ -65,7 +64,7 @@ func _physics_process(delta):
 		var other_flask : ConicalFlask = colider
 		#print("known ids nuts : ", known_ids, " my id : ", id)
 		
-		if other_flask.id not in known_ids and is_below(other_flask):
+		if other_flask.id not in known_ids and is_below(other_flask) and is_facing_up():
 			print("pouring into flask")
 			other_flask.color = color.lerp(other_flask.color,0.5)
 			other_flask.set_color()
@@ -75,15 +74,12 @@ func _physics_process(delta):
 			var new_id = other_flask.generate_id()
 			other_flask.id = new_id
 			known_ids[new_id] = true
-			if id in other_flask.known_ids:
+			if id in other_flask.known_ids: # I don't think this works
 				other_flask.known_ids.erase(id)
 			
 			if debug_show_id:
 				$Label3D2.text = "known ids nuts : " + str(known_ids)
-		else :
-			if delete_me != other_flask.id:
-				print(other_flask.id)
-			delete_me = other_flask.id
+
 			
 func generate_id() -> int:
 	id = randi()
@@ -99,3 +95,16 @@ func is_below(other_object: Node3D) -> bool:
 	
 	# Compare Y coordinates (in Godot, Y axis points up)
 	return my_position.y > other_position.y
+
+func is_facing_up() -> bool:
+	var object_up = global_transform.basis.y
+	var angle = object_up.angle_to(Vector3.UP)
+	#var angel = Vector3.UP.angle_to(global_position)
+	return angle < PI/1.8
+
+func is_facing_down() -> bool:
+	var object_up = global_transform.basis.y
+	var angle = object_up.angle_to(Vector3.UP)
+	
+	# If angle is greater than 90 degrees (PI/2), it's roughly facing down
+	return angle > PI/2.5
