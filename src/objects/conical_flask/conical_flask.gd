a895dcd360149a1664f9b2fe3811c7477728e025
Viktor Kolind <s204166@student.dtu.dk>
2025-02-17 20:37:44 +0100

    added basic mixing

--- src/objects/conical_flask/conical_flask.gd
+++ src/objects/conical_flask/conical_flask.gd
@@ -1,20 +1,23 @@
 class_name ConicalFlask extends RigidBody3D
 
+@export var color : Vector3
+@export var debug_show_id := false
+
 @onready var beaker = $beaker
-@onready var liquid : GPUParticles3D = $Liquid
-
-var shader_material : ShaderMaterial = preload("res://models/liquid_materials/color_changer.tres")
-@export var color : Vector3 : 
-	set(val):
-		_set_color()
-		color = val
-		
-
 @onready var raycast : RayCast3D = $RayCast3D
+@onready var label : Label3D = $Label3D
+
+var liquid_scene := preload("res://src/objects/liquid/liquid.tscn")
+var liquid : GPUParticles3D
+var shader_material : ShaderMaterial = preload("res://models/liquid_materials/color_changer.tres")
+
 
 # id is used for mix chemicals so you don't two chemicals twice in a loop
 var id : int
+var last_mixed_with_id: int = -1  # -1 means never mixed
 var known_ids : Dictionary = {}
+
+var delete_me = 0
 
 var draw_particles : bool : 
 	set(val) : 
@@ -22,66 +25,76 @@ var draw_particles : bool :
 			liquid.restart()
 		liquid.visible = val
 		
-#@onready var chemical = $beaker/juice
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	_set_color()
+	id = generate_id()
+	set_color()
+	
 
-func _set_color():
+func set_color():
 	if not is_node_ready():
 		return
 	var chemical : MeshInstance3D = beaker.get_node("juice")
-	var dupe := shader_material.duplicate()
-	chemical.mesh.surface_get_material(0).next_pass = dupe
-	var particle_process_material : ParticleProcessMaterial = ParticleProcessMaterial.new()
-	chemical.set_surface_override_material(0, chemical.mesh.surface_get_material(0).duplicate())
+	var material := shader_material.duplicate(true)
+	material.set_shader_parameter("color", color)
+	chemical.set_surface_override_material(0, material)
 
-	#chemical.mesh.set_surface_override_material(0, chemical.mesh.surface_get_material(0).duplicate())
-	#particle_process_material.color = 
+	for child in get_children():
+		if child is GPUParticles3D:
+			remove_child(child)
+	
+	liquid = liquid_scene.instantiate().duplicate()
+	liquid.process_material = liquid.process_material.duplicate(true)
+	add_child(liquid)
 	liquid.process_material.color = Color(color.x,color.y,color.z,1.0)
 	liquid.visible = false
-	dupe.set_shader_parameter("color", color)
-
+	print("color changed ", color)
+	
 func _physics_process(delta):
 	if rotation_degrees.z > 90.0 and rotation_degrees.z < 270.0:
-		#liquid.restart()
 		draw_particles = true
 		raycast.enabled = true
 	else : 
-		#liquid.restart()
 		draw_particles = false
 		raycast.enabled = false
 		
 	# pouring logic
 	raycast.look_at(raycast.global_transform.origin + Vector3(0, 0, -10), Vector3.UP)
 	var colider = raycast.get_collider()
-	if colider != null and colider is ConicalFlask:
-		
+	if colider != null and colider is ConicalFlask :
 		var other_flask : ConicalFlask = colider
-		if other_flask.id not in known_ids:
+		#print("known ids nuts : ", known_ids, " my id : ", id)
+		
+		if other_flask.id not in known_ids and is_below(other_flask):
 			print("pouring into flask")
-			
-			other_flask.color = color * other_flask.color
-			var new_id = randi()
+			other_flask.color = color.lerp(other_flask.color,0.5)
+			other_flask.set_color()
+			#known_ids[other_flask.id] = false
+			#other_flask.known_ids.clear()
+			#known_ids[other_flask.id] = false
+			var new_id = other_flask.generate_id()
 			other_flask.id = new_id
 			known_ids[new_id] = true
+			if id in other_flask.known_ids:
+				other_flask.known_ids.erase(id)
+			
+			if debug_show_id:
+				$Label3D2.text = "known ids nuts : " + str(known_ids)
+		else :
+			if delete_me != other_flask.id:
+				print(other_flask.id)
+			delete_me = other_flask.id
+			
+func generate_id() -> int:
+	id = randi()
+	if debug_show_id:
+		label.text = str(id)
+	#known_ids.clear()
+	return id
+
+func is_below(other_object: Node3D) -> bool:
+	# Get the global positions of both objects
+	var my_position = global_position
+	var other_position = other_object.global_position
 	
-func get_down_angle() -> float:
-	var forward_direction = -global_transform.basis.z
-	var down_vector = Vector3.DOWN
-	return rad_to_deg(forward_direction.angle_to(down_vector))
-	
-# Assuming your object is a Node3D (or any derived class like CharacterBody3D)
-func is_facing_downward() -> bool:
-	# Get the object's forward direction (negative Z axis)
-	var forward_direction = -global_transform.basis.z
-	
-	# Global down vector
-	var down_vector = Vector3.DOWN  # Same as Vector3(0, -1, 0)
-	
-	# Get the dot product between the forward direction and down vector
-	var dot_product = forward_direction.dot(down_vector)
-	
-	# If dot product is positive, the object is facing more downward than upward
-	# (angle between vectors is less than 90 degrees)
-	return dot_product > 0
+	# Compare Y coordinates (in Godot, Y axis points up)
+	return my_position.y > other_position.y
