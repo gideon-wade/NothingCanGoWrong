ae94f941857d35b2b59bf0813f6093498e8b9a77
Gideon <gidewadejob@gmail.com>
2025-02-18 19:43:46 +0100

    tasks update

--- src/main/task.gd
+++ src/main/task.gd
@@ -1,17 +1,17 @@
 class_name Task extends Object
 var description : String
 var complete : bool = false
-var goalHexCode : String
+var substance_name : String
 var id : int
 var steps = []
 
-func _init(_description : String, _goalHexCode : String, _id : int) -> void:
+func _init(_description : String, _substance_name : String, _id : int) -> void:
 	description = _description
-	goalHexCode = _goalHexCode
+	substance_name = _substance_name
 	id = _id
 	GameMaster.conical_changed.connect(_update_task)
 
-func _update_task(hexcode : String) -> void:
-	if goalHexCode == hexcode:
+func _update_task(_substance_name : String) -> void:
+	if substance_name == _substance_name:
 		complete = true
 		GameMaster.task_complete(id)
--- src/main/task_giver.gd
+++ src/main/task_giver.gd
@@ -2,10 +2,13 @@ class_name TaskGiver extends Node
 var tasks : Array[Task] = []
 
 func _ready():
-	var task1 = Task.new("Mix red with yellow to get an orange substance", "FF5E00", 1)
-	task1.steps.append(["Red", "Blue"])
-	var task2 = Task.new("Mix black with white to get a grey substance", "808080", 2)
-	var task3 = Task.new("Mix red with white to get a pink substance", "FFAEC9", 3)
+	var task1 = Task.new("Mix Oceamid with Redamin to get Grass Juice", "Grass Juice", 1)
+	var task2 = Task.new("Mix Grass Juice with Apple Juice to get Cadrulium", "Cadrulium", 2)
+	var task3 = Task.new("Mix Cadrulium with Grass Juice to get Princiade", "Princiade", 3)
+	var task4 = Task.new("Mix Princiade with Oceamid to get Dracula's Blood", "Dracula's Blood", 4)
+	var task5 = Task.new("Mix Dracula's Blood with Apple Juice to get Laudle", "Laudle", 5)
 	tasks.append(task1)
 	tasks.append(task2)
 	tasks.append(task3)
+	tasks.append(task4)
+	tasks.append(task5)
